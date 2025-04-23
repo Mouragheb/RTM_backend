3ba07b9844a2ceda85eda54960fa2cde8714a827
mousragheb <ragheb.moustafa@yahoo.com>
2025-04-23 15:38:32 -0500

    making sure to make an upload directory if it doesnt exist

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path'); // <-- ADD THIS LINE
+
 
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 // Middleware
 app.use(express.json());
 app.use(cors());
